cb115f57641d65b0a8d296f4cf9297ad4e3da8d9
nopost <nopost@qq.com>
2019-04-06 17:02:17 +0800

    Create proxy_init.sh

---
+++ container-files/config/init/proxy_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#export_proxy
+export ftp_proxy=http://192.168.2.250:3128 
+export http_proxy=http://192.168.2.250:3128
+export https_proxy=http://192.168.2.250:3128
+#profile_proxy
+all_proxy="all_proxy=socks://192.168.2.250:1080"
+ftp_proxy="ftp_proxy=http://192.168.2.250:3128"
+http_proxy="http_proxy=http://192.168.2.250:3128"
+https_proxy="https_proxy=http://192.168.2.250:3128"
+no_proxy="no_proxy=localhost,127.0.0.0/8,::1,.xxx.intra" 
+for var in "$all_proxy" "$ftp_proxy" "$http_proxy" "$https_proxy" "$no_proxy";do
+      echo $var >> /etc/profile
+done 
+
+for var in "all_proxy" "ftp_proxy" "http_proxy" "https_proxy" "no_proxy";do
+      echo "export $var" >> /etc/profile
+done
+source /etc/profile
