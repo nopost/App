34c57cb1464d9838d40483b40d86092b053f1206
nopost <nopost@qq.com>
2019-04-06 17:00:59 +0800

    Update base_init.sh

--- container-files/config/init/base_init.sh
+++ container-files/config/init/base_init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-export ftp_proxy=http://192.168.2.250:3128 
-export http_proxy=http://192.168.2.250:3128
-export https_proxy=http://192.168.2.250:3128
 chmod +x /config/npc/npc
 chmod +x /config/proxy/proxy
-
