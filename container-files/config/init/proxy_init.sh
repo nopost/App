94f14e9faf4bf6f27f1f2216027714d559d71b81
nopost <nopost@qq.com>
2019-04-08 14:44:46 +0800

    Update proxy_init.sh

--- container-files/config/init/proxy_init.sh
+++ container-files/config/init/proxy_init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #profile_proxy
-
-
-export all_proxy="socks://192.168.2.250:1080"
-export ftp_proxy="http://192.168.2.250:3128"
-export http_proxy="http://192.168.2.250:3128"
-export https_proxy="http://192.168.2.250:3128"
-export no_proxy="localhost,127.0.0.0/8,::1,.xxx.intra" 
+if [ -z "$HTTP_PROXY" ]; then
+  export http_proxy="$HTTP_PROXY"
+  export https_proxy="$HTTP_PROXY"
+fi
