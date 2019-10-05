50bde531b83c523fbce92eeb9e9ebeb2a3d6d253
nopost <nopost@qq.com>
2019-10-05 16:31:37 +0800

    Update 003_set_sshd.sh

--- container-files/install/build/003_set_sshd.sh
+++ container-files/install/build/003_set_sshd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key && \ 
 ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key && \ 
 ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key && \ 
