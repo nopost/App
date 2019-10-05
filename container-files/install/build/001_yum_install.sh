0f1eb2cd9716e9dc6b7d5dcd5521241d5d511141
nopost <nopost@qq.com>
2019-10-05 16:13:23 +0800

    Create 001_yum_install.sh

---
+++ container-files/install/build/001_yum_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#mkdir -p /var/run/sshd && \
+#yum update -y && \
+yum install -y \
+    iproute \
+    python-setuptools \
+    hostname \
+    inotify-tools \
+    yum-utils \
+    which \
+    wget \
+    net-tool \
+    jq \
+    openssh-server  \ 
+    openssh-clients \
+    rsync
