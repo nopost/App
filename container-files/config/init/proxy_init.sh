fbd0ccfbf1a3d6b3f33673ff10807702eca59ba0
nopost <nopost@qq.com>
2019-10-05 12:30:26 +0800

    Update proxy_init.sh

--- container-files/config/init/proxy_init.sh
+++ container-files/config/init/proxy_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #profile_proxy
-if [ -z "$HTTP_PROXY" ]; then
-  export http_proxy="$HTTP_PROXY"
-  export https_proxy="$HTTP_PROXY"
+if [ -z "$HTTP_PROXY" ]; then 
+  export proxy="$HTTP_PROXY"
+  export http_proxy=$proxy
+  export https_proxy=$proxy
+  export ftp_proxy=$proxy
+  export no_proxy="localhost, 127.0.0.1, ::1"
 fi
